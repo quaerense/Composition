0e5ca151440727365469c37701e8f7f96b0bcbfe
Alexey Bakaykin <color.ui>
2022-01-29 17:01:01 +0300

    Add fragments and resource files

---
+++ app/src/main/java/org/quaerense/composition/presentation/WelcomeFragment.kt
@@ -0,0 +1,19 @@
+package org.quaerense.composition.presentation
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import org.quaerense.composition.R
+
+class WelcomeFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_welcome, container, false)
+    }
+}
